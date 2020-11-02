9b35422776de34227cfad4bc8ce9a790272880ad
Tom Renard <tom.renard@zoho.com>
2020-11-02 18:47:58 +0100

    migration issue

--- db/migrate/20201031184018_delete_image_to_article.rb
+++
@@ -1,4 +0,0 @@
-class DeleteImageToArticle < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/migrate/20201031184136_remove_image_to_article.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageToArticle < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :articles, :image, :string
-  end
-end
