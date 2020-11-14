e1ec0718988d6ecdd3c0a69d1bf2bb17dc3fde87
Tom Renard <tom.renard@zoho.com>
2020-11-14 19:22:07 +0100

    index page detection infinite scrolling

--- app/javascript/components/infinite_scroll.js
+++ app/javascript/components/infinite_scroll.js
@@ -1,9 +1,11 @@
 const initInfiniteScroll = () => {
   function infiniteMove() {
+    const p = window.location.pathname;
+    console.log(p);
     const winheight = window.innerHeight;
     const winoffset = window.pageYOffset;
     const scrollH = document.body.scrollHeight;
-    if (winheight + winoffset >= scrollH)
+    if ((p.length === 0 || p === "/" || p.match(/^\/?index/)) && (winheight + winoffset) >= scrollH)
       scroll(0,0);
   }
   window.addEventListener('scroll', infiniteMove);
